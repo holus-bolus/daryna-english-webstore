86597c6f806b71dd4e0079e62e1f4bc7bc8c9bfa
holus-bolus <ustinovoleksij@gmail.com>
2023-12-30 14:51:36 +0200

    Added functions for the cart

--- src/components/Cart.tsx
+++ src/components/Cart.tsx
@@ -1,5 +1,46 @@
+import { useDispatch, useSelector } from 'react-redux';
+import { RootState } from '../app/store';
+import { removeProduct, updateQuantity } from '../features/cart/cartSlice'; // Removed .ts extension
+
 const Cart = () => {
-  return <div></div>;
+  const cartItems = useSelector((state: RootState) => state.cart.items);
+  const dispatch = useDispatch();
+
+  const handleRemove = (id: number) => {
+    dispatch(removeProduct(id));
+  };
+
+  const handleChangeQuantity = (id: number, quantity: number) => {
+    if (quantity < 1) {
+      dispatch(removeProduct(id));
+    } else {
+      dispatch(updateQuantity({ id, quantity }));
+    }
+  };
+
+  return (
+    <div>
+      <h2>Cart</h2>
+      {cartItems.map((item) => (
+        <div key={item.id}>
+          <h3>{item.name}</h3>
+          <p>
+            Quantity:
+            <input
+              type="number"
+              value={item.quantity}
+              onChange={(e) =>
+                handleChangeQuantity(item.id, parseInt(e.target.value))
+              }
+              min="1"
+            />
+          </p>
+          <p>Price: ${item.price}</p>
+          <button onClick={() => handleRemove(item.id)}>Remove</button>
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default Cart;
--- src/features/cart/cartSlice.ts
+++ src/features/cart/cartSlice.ts
@@ -1,5 +1,5 @@
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 import { CartItem } from '../../types';
-import { createSlice } from '@reduxjs/toolkit';
 
 interface CartState {
   items: CartItem[];
@@ -13,22 +13,33 @@ export const cartSlice = createSlice({
   name: 'cart',
   initialState,
   reducers: {
-    addProduct: (state, action) => {
-      const item = state.items.find((item) => item.id === action.payload.id);
-      if (item) {
-        item.quantity++;
+    addProduct: (state, action: PayloadAction<CartItem>) => {
+      const existingItem = state.items.find(
+        (item) => item.id === action.payload.id,
+      );
+      if (existingItem) {
+        existingItem.quantity++;
       } else {
         state.items.push({ ...action.payload, quantity: 1 });
       }
     },
-    removeProduct: (state, action) => {
-      const item = state.items.find((item) => item.id === action.payload.id);
-      if (item) {
-        item.quantity--;
+    removeProduct: (state, action: PayloadAction<number>) => {
+      state.items = state.items.filter((item) => item.id !== action.payload);
+    },
+    updateQuantity: (
+      state,
+      action: PayloadAction<{ id: number; quantity: number }>,
+    ) => {
+      const existingItem = state.items.find(
+        (item) => item.id === action.payload.id,
+      );
+      if (existingItem) {
+        existingItem.quantity = action.payload.quantity;
       }
     },
   },
 });
 
-export const { addProduct, removeProduct } = cartSlice.actions;
+export const { addProduct, removeProduct, updateQuantity } = cartSlice.actions;
+
 export default cartSlice.reducer;
