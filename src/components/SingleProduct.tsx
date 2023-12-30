ef42270904a7a281e10cc759e2b91bbf9e634181
holus-bolus <ustinovoleksij@gmail.com>
2023-12-30 18:09:56 +0200

    Added the build

--- src/components/SingleProduct.tsx
+++ src/components/SingleProduct.tsx
@@ -18,22 +18,24 @@ const SingleProduct: React.FC<SingleProductProps> = ({ product }) => {
 
   return (
     <div className="col mb-4">
-      <div className="card h-100">
+      <div className="card h-100 d-flex flex-column">
         <img
           src={product.imageUrl}
           className="card-img-top"
           alt={product.name}
           style={{ height: '600px', objectFit: 'cover' }}
         />
-        <div className="card-body">
+        <div className="card-body d-flex flex-column">
           <h5 className="card-title">{product.name}</h5>
-          <p className="card-text">{product.description}</p>
-          <p className="card-text">
-            <strong>Price:</strong> ₴{product.price}
-          </p>
-          <button className="btn btn-primary" onClick={handleAddToCart}>
-            Add to cart
-          </button>
+          <p className="card-text flex-grow-1">{product.description}</p>
+          <div className="mt-auto">
+            <p className="card-text">
+              <strong>Price:</strong> ₴{product.price}
+            </p>
+            <button className="btn btn-primary" onClick={handleAddToCart}>
+              Add to cart
+            </button>
+          </div>
         </div>
       </div>
     </div>
