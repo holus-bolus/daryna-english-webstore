c90443710d3c924e274208718e883b613dfaa0c8
holus-bolus <ustinovoleksij@gmail.com>
2023-12-30 17:12:35 +0200

    Added the About page

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -2,7 +2,7 @@ import NavigationMenu from './NavigationMenu.tsx';
 
 const Home = () => {
   return (
-    <div>
+    <div className="container">
       <NavigationMenu />
       <h1>Home</h1>
       <p>This is the home page</p>
--- src/components/Products.tsx
+++ src/components/Products.tsx
@@ -1,6 +1,7 @@
 import { useSelector } from 'react-redux';
 import { RootState } from '../app/store.ts';
 import SingleProduct from './SingleProduct.tsx';
+import NavigationMenu from './NavigationMenu.tsx';
 
 const Products = () => {
   const products = useSelector((state: RootState) => state.products.items);
@@ -8,6 +9,7 @@ const Products = () => {
   return (
     <>
       <div className="container">
+        <NavigationMenu />
         <div className="row">
           {products &&
             products.map((product) => (
