45d1b08c29b44f7a69e76a9582fee774fa04fc23
holus-bolus <ustinovoleksij@gmail.com>
2023-12-30 18:02:29 +0200

    Added the build

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,9 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+const base = process.env.BASE_URL || '/';
+
 export default defineConfig({
   plugins: [react()],
-  base: '/daryna-english-webstore/',
+  base, // Set the base to '/'
 });
